a39719d71e041edfca9b68e689b6a97d394f7c34
Kinga Mazur <kmzr9b67@gmail.com>
2024-12-06 19:07:10 +0100

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cstdlib>
-
+#include <ctime>
 using namespace std;
 
 int main() {
     int a ;
+    srand(time(NULL));
     int b = rand()%100+1;
     cout << "Enter the value: ";
     cin >> a;
-    cout << b << "\n";
+    cout <<b << "\n";
     int my_arr[b];
     for (int i=0; i < a; i++) my_arr[i] = rand()%100-100;
-    for (int i=0; i<sizeof(my_arr)/sizeof(my_arr[0]);i++) {
+    for (int i=0; i< sizeof(my_arr)/sizeof(my_arr[0]); i++) {
         if (my_arr[i] == a) {
             cout << i << '\n';
             return 0;
