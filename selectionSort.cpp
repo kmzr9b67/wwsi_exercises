9118398317b83706693115dcadfd322d918743f9
Kinga Mazur <kmzr9b67@gmail.com>
2024-12-10 21:24:01 +0100

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int my_arr[] = {1,3, 8, 25,2,18, 0, -19, 14, 22, -21, -100, -32};
+int my_arr[] = {1, 2, 3, -4, 8, 25,-200, 2, 215,18, 0, -19, 14, 22, -21, -100, -32};
 int main() {
-    int temp;
-    int zap, max, wyk;
-    for (int i=0; i <sizeof(my_arr)/sizeof(my_arr[0]); i++ ) {
-        for (int j=0; j <sizeof(my_arr)/sizeof(my_arr[0])-i; j++ ) {
-            if (j == 0 or my_arr[j]>max) {
-                max = my_arr[j];
-                zap = j;
+    int n = sizeof(my_arr)/sizeof(my_arr[0]);
+
+    for (int i=0; i <n; i++ ) {
+        int max_index = 0;
+        int max_value = my_arr[0];
+        for (int j=1; j <n-i; j++ ) {
+            if (my_arr[j]>max_value) {
+                max_value = my_arr[j];
+                max_index = j;
             }
         }
-        temp = my_arr[sizeof(my_arr)/sizeof(my_arr[0])-i-1];
-        my_arr[sizeof(my_arr)/sizeof(my_arr[0])-i-1] = max;
-        my_arr[zap] = temp;
+        swap(my_arr[n-i-1], my_arr[max_index]);
     }
     cout << "String" << "\n";
     for (int i : my_arr) {
